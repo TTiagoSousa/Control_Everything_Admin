9c3e1d5f924d8a0fb9ce023edc96c57121989bc1
Tiago Sousa <ttiagopintosousa97@gmail.com>
2024-03-13 03:18:35 +0000

    Header Index

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import './App.scss';
 import { Navigate, Route, Routes, useLocation } from 'react-router-dom';
 import * as Public_Page from './Imports/public.pages';
+import Header_Index from './Containers/Headers/Header_Index/Header_Index';
 
 function App() {
 
   return (
     <main>
+
+      <Header_Index />
+
       <Routes>
         <Route index element={<Public_Page.Index />} />
       </Routes>
---
+++ src/Containers/Headers/Header_Index/Header_Index.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Header_Index.scss';
+
+const Header_Index = () => {
+  return (
+    <header className='Header_Index'>
+      <div className='Left_Side'>
+        <h1>Control Everything</h1>
+      </div>
+      <div className='Rigth_Side'></div>
+    </header>
+  )
+};
+
+export default Header_Index;
