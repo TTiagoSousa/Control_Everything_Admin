f264b0af36e292950e93e5448c68005e3bbed82a
Tiago Sousa <ttiagopintosousa97@gmail.com>
2024-03-24 02:45:53 +0000

    Apis dashboard translations

--- src/Pages/Private/Apis/Apis_Home/Containers/Countries_Api_Card.jsx
+++ src/Pages/Private/Apis/Apis_Home/Containers/Countries_Api_Card.jsx
@@ -1,14 +1,17 @@
 import React from 'react';
 import '../Apis_Home.scss';
 import { Link } from 'react-router-dom'
+import { useTranslation } from 'react-i18next';
 
 const Countries_Api_Card = () => {
+
+  const { t } = useTranslation();
 
   return (
     <div className='Card'>
       <div className='wrappe'>
         <div className='Tittle'>
-          <span>Countries Api</span>
+          <span>{t("Countries API")}</span>
         </div>
         <div className='Information'>
           <Link to="Countries_Api">Api Dasboard</Link>
